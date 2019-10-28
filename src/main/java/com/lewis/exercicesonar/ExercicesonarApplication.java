abca25968b96ea58cef282ca0b34cdcb469c899c
louishellapro@gmail.com <LewisDev123!>
2019-10-28 11:18:09 +0100

    creation projet

---
+++ src/main/java/com/lewis/exercicesonar/ExercicesonarApplication.java
@@ -0,0 +1,13 @@
+package com.lewis.exercicesonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicesonarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExercicesonarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lewis/exercicesonar/ExercicesonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewis.exercicesonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicesonarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
